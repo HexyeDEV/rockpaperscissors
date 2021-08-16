eb43cefba05c256f997d0160dfd2f2ef8c5fd999
Hexye <65314629+HexyeDEV@users.noreply.github.com>
2021-08-16 17:32:31 +0200

    just little cleaner

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -12,9 +12,7 @@ while True:
     if user_input not in options:
         continue
 
-    random_number = random.randint(0, 2)
-    # rock: 0, paper: 1, scissors: 2
-    computer_pick = options[random_number]
+    computer_pick = random.choice(options)
 
     print("Computer picked", computer_pick + ".")
 
